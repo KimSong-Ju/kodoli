7e1c20d513a789f1090725d1d93e5f7f9a1d2c75
KimSongJu <kodoli@nate.com>
2022-04-20 17:09:06 +0900

    첫번째 스터디용

---
+++ test/study/ScoreCollectionTest.java
@@ -0,0 +1,23 @@
+package study;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.Test;
+
+public class ScoreCollectionTest {
+
+	@Test
+	public void answersArithmeticMeanOfTwoNumbers() {
+		// 준비
+		ScoreCollection collection = new ScoreCollection();
+		collection.add(()->5);
+		collection.add(()->7);
+		
+		// 실행
+		int actualResult = collection.arithmeticMean();
+		
+		// 단언
+		assertThat(actualResult, equalTo(6));
+	}
+
+}
